457cadf608fcb18f1b349588f5ad6a425985e1d3
Melissa G <jstmac@gmail.com>
2019-05-28 14:25:37 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var title = document.getElementById("title");
+title.addEventListener("click", function() {
+    debugger
+});
+
+var options = ["rock", "paper", "scissors"];
+var userChoice; 
+var compChoice;
+
+var choices = document.getElementsByClassNane("choice");
+console.log(choices);
+choices.( function(choice) {
+    choice.addEventListener("click", functional(){
+
+    }
+})
+
+//get the cop choice
+//display choices
+//figure out the winner
+
